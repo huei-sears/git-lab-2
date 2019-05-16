921b425516b5f5a7449b2913e241e772c5895c17
Huei Sears <hs549118@ohio.edu>
2019-05-16 15:21:34 -0400

    Printing Phrase

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Huei Sears
+ *        Date: 05/16/2019
+ * Description: Printing 'Hello Git!'
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout <<"Hello Git!!" << endl;
+    return 0;
+}// main
